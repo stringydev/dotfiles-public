b7e8185f5007435fa463cd90c855c407c9e591e2
The Great Gravitist <107803920+thegreatgravitist@users.noreply.github.com.>
2024-10-24 20:13:31 +0100

    fix(fish): redefined remove_path in /functions

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,53 +1,43 @@
+# Check if the shell is interactive
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set fish_greeting ""
+# Basic settings
+set fish_greeting ""  # Disable the default fish greeting
+set -gx TERM xterm-256color  # Set terminal type for color support
 
-set -gx TERM xterm-256color
-
-# theme
+# Theme configuration
+# Choose a theme for the shell prompt
 fish_config theme choose "Rosé Pine"
-set -g fish_prompt_pwd_dir_length 1
-set -g theme_display_user yes
-set -g theme_hide_hostname no
-set -g theme_hostname always
+set -g fish_prompt_pwd_dir_length 1  # Set the length of the current directory in prompt
+set -g theme_display_user yes  # Display the user in the prompt
+set -g theme_hide_hostname no  # Always show hostname in the prompt
+set -g theme_hostname always  # Show hostname in prompt
 
-# aliases
-alias g git
-command -qv nvim && alias vim nvim
+# Aliases
+alias g git  # Shorten git command
+command -qv nvim && alias vim nvim  # Use nvim as vim if available
+
 if type -q eza
-    alias ll "eza -l -g --icons"
-    alias lla "ll -a"
+    alias ll "eza -l -g --icons"  # Use eza for detailed listing
+    alias lla "ll -a"  # Show all files with eza
 end
 
-# editor
-set -gx EDITOR nvim
+# Set the default editor
+set -gx EDITOR nvim  # Use nvim as the default editor
 
-# reload fish
-bind \cx reload_fish
+# Keybindings
+bind \cx reload_fish  # Reload the fish configuration with Ctrl + x
 
-# Fzf
-fzf_configure_bindings --directory=\cf
-set fzf_directory_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"
-bind \cn fzf_change_directory
+# Fzf configuration
+fzf_configure_bindings --directory=\cf  # Set fzf to change directories with Ctrl + f
+set fzf_directory_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"  # Open selected file in editor
+bind \cn fzf_change_directory  # Change directory using fzf with Ctrl + n
 
-# Pyenv root path
-set -Ux PYENV_ROOT "$HOME/Developer/.pyenv"
-pyenv init - | source
+# Pyenv configuration
+set -Ux PYENV_ROOT "$HOME/Developer/.pyenv"  # Set Pyenv root path
+pyenv init - | source  # Initialize Pyenv
 
-# remove path
-function removepath
-    if set -l index (contains -i $argv[1] $PATH)
-        set --erase --universal fish_user_paths[$index]
-        echo "Updated PATH: $PATH"
-    else
-        echo "$argv[1] not found in PATH: $PATH"
-    end
-end
-
-# Created by `pipx` on 2024-10-24 16:10:40
-set PATH $PATH /Users/ttoe/.local/bin
-
-# Poetry config file path
-set -Ux POETRY_CONFIG_DIR $HOME/.config/pypoetry/
+# Poetry configuration
+set -Ux POETRY_CONFIG_DIR $HOME/.config/pypoetry/  # Set the Poetry config file path
